df3698765f9121d5b2370a84f2afdf5d7ffc448b
Pierre-Yves Aillet <pyaillet@gmail.com>
2018-09-17 22:31:07 +0200

    :sparkles: Shiny demos

--- godemo/2/server.go
+++ godemo/2/server.go
@@ -5,15 +5,35 @@ import (
     "log"
     "net/http"
     "os"
+    "strings"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func getMessage() string {
     host, _ := os.Hostname()
-    fmt.Fprintf(w, "V2 works on %s!\n", host)
+    return fmt.Sprintf("V2 works on %s!\n", host)
+}
+
+func htmlHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<html><body><h3>%s</h3><img style=\"width:50%%\" src=\"static/demo1.png\" /></body></html>\n", getMessage())
+}
+
+func textHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, getMessage())
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    ua := r.Header.Get("User-agent")
+    if strings.HasPrefix(ua, "curl") {
+        textHandler(w, r)
+    } else {
+        htmlHandler(w, r)
+    }
 }
 
 func main() {
     http.HandleFunc("/", handler)
+    fs := http.FileServer(http.Dir("static/"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
     log.Fatal(http.ListenAndServe(":80", nil))
 }
 
